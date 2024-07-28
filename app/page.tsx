d06ef0bd8a04217b3a008f0db84fd2be4a78e95e
AmRo <VirtualEmailBox045@Gmail.Com>
2024-07-28 13:44:33 +0330

    Create the basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,99 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="bg-white text-black flex justify-center font-primary font-normal">
+            <section className="w-a4 h-a4" id="container">
+                <div className="w-[640px] m-auto">
+                    <div className="flex justify-end  text-sm">
+                        <div className="text-end" contentEditable={true}>
+                            <div>Datum : 15.03.2024</div>
+                            <div>Rechnungs-Nr.: 240005</div>
+                        </div>
+                    </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                    <div className="mt-[8rem]" contentEditable={true}>
+                        <div>Firma Berg GMBH</div>
+                        <div>HEIZUNG-KLIMA</div>
+                        <div>SANITÄR KLEMPNER</div>
+                        <div>Ostfeld 15</div>
+                        <div>21635 Jork</div>
+                    </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                    <div className="my-5">
+                        <strong className="font-semibold text-xl">Rechnung</strong>
+                    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                    <div className="mb-4" contentEditable={true}>
+                        <div>Sehr geehrte Damen und Herren,</div>
+                        <div>ich danke Ihnen für die Beauftragung. Wie vereinbart</div>
+                        <div>erhalten Sie die Rechnung für meine Leistung von 07.03.2024 bis 15.03.2024.</div>
+                    </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                    <div>
+                        <table className="w-full">
+                            <thead>
+                            <tr className="bg-[#e7e6e6] text-left px-2">
+                                <th className="text-center px-1 font-normal">Pos.</th>
+                                <th className="text-center font-normal">Anzahl</th>
+                                <th className="text-center font-normal">Einheit</th>
+                                <th className="w-max text-start font-normal">Bezeichnung</th>
+                                <th className="w-max text-end font-normal">Einzelpreis</th>
+                                <th className="w-max text-end font-normal pe-2">Gesamtpreis</th>
+                            </tr>
+                            </thead>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                            <tbody>
+                            <tr className="text-start">
+                                <td className="text-center px-1">1</td>
+                                <td className="text-center px-1">50</td>
+                                <td className="text-center min-w-[70px]">Std.</td>
+                                <td className="text-[#0070c0] text-start min-w-[60px]">Monteurleistung</td>
+                                <td className="text-[#0070c0] text-end">50.00 €</td>
+                                <td className="text-[#0070c0] text-end pe-2">2,500.00 €</td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                        <div className="grid gap-3 mb-5 mt-[2.5rem]">
+                            <div className="flex justify-between px-2">
+                                <span>Summe</span>
+                                <span className="text-[#0070c0]" contentEditable={false}>2,500.00 €</span>
+                            </div>
+
+                            <div className="flex justify-between px-2">
+                                <span>Mehrwertsteuer 19% auf den Nettobedivag</span>
+                                <span className="text-[#0070c0]" contentEditable={false}>475.00 €</span>
+                            </div>
+
+                            <div className="bg-[#e7e6e6] flex justify-between px-2">
+                                <span className="">Gesamtbedivag</span>
+                                <span className="text-[#0070c0] font-bold" contentEditable={false}>2,975.00 €</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="px-1">
+                        <div className="grid gap-5">
+                            <div>Der Leistungsempfänger schuldet die Umsatzsteuer nach § 13b UStG.</div>
+                            <div>Der Rechnungsbetrag (2,637.50 EUR) ist fällig bis zum 11.07.2024.</div>
+                            <div>
+                                <div>Mit freundlichen Grüßen.</div>
+                                <div>Alireza Divdar</div>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between mt-[40px] text-sm" contentEditable={true}>
+                            <div>Bankverbindung:</div>
+                            <div>Steuernummer: 43/109/02528</div>
+                        </div>
+
+                        <div className="text-sm">
+                            <div contentEditable={true}>Sparkasse Stade-Altes Land</div>
+                            <div contentEditable={true}>BAN: DE69 2415 1005 1210 4192 20</div>
+                            <div contentEditable={true}>BIC: NOLADE21STS</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 }
