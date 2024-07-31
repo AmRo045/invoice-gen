80b5933a628d77ac7ab1182e0af682b19e39660c
AmRo <VirtualEmailBox045@Gmail.Com>
2024-07-31 10:54:42 +0330

    Add logic

---
+++ app/components/modal.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import { InvoiceItem } from "@/app/page";
+
+interface Props {
+    newPos: number;
+    onAdd: (record: InvoiceItem) => void;
+}
+
+export default function Modal({ newPos, onAdd }: Props) {
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+    const form = useForm<InvoiceItem>({
+        defaultValues: {
+            pos: newPos
+        }
+    });
+
+    const actualSubmit = (data: InvoiceItem) => {
+        if (onAdd) {
+            onAdd(data);
+        }
+        form.reset();
+        dialogRef.current?.close();
+    };
+
+    const openModal = () => {
+        dialogRef.current?.showModal();
+    };
+
+    useEffect(() => {
+        const handleBackdropClick = (event: MouseEvent) => {
+            if (dialogRef.current && event.target === dialogRef.current) {
+                dialogRef.current?.close();
+            }
+        };
+
+        const dialogElement = dialogRef.current;
+        if (dialogElement) {
+            dialogElement.addEventListener("click", handleBackdropClick);
+        }
+
+        return () => {
+            if (dialogElement) {
+                dialogElement.removeEventListener("click", handleBackdropClick);
+            }
+        };
+    }, []);
+
+    return <>
+        <button
+            onClick={openModal}
+            className="bg-black text-white rounded-lg font-bold py-2 px-4 hover:bg-gray-700 min-w-fit">+ Add
+        </button>
+
+        <dialog ref={dialogRef} className="rounded-lg">
+            <form className="p-3" onSubmit={form.handleSubmit(actualSubmit)}>
+                <h1>Add record</h1>
+                <hr className="mb-3 mt-2" />
+                <div className="my-2 flex justify-between items-center">
+                    <span>Pos.</span>
+                    <input type="number" className="border-2 rounded-lg ms-2 p-1" {...form.register("pos", {
+                        required: false,
+                        setValueAs: (v) => parseInt(v)
+                    })} />
+                </div>
+
+                <div className="my-2 flex justify-between items-center">
+                    <span>Anzahl</span>
+                    <input type="number" className="border-2 rounded-lg ms-2 p-1" {...form.register("hours", {
+                        required: false,
+                        setValueAs: (v) => parseInt(v)
+                    })} />
+
+                </div>
+
+                <div className="my-2 flex justify-between items-center">
+                    <span>Einheit</span>
+                    <input type="text" className="border-2 rounded-lg ms-2 p-1" {...form.register("unit", {
+                        required: false
+                    })} />
+                </div>
+
+                <div className="my-2 flex justify-between items-center">
+                    <span>Bezeichnung</span>
+                    <input type="text" className="border-2 rounded-lg ms-2 p-1" {...form.register("description", {
+                        required: false
+                    })} />
+                </div>
+
+                <div className="my-2 flex justify-between items-center">
+                    <span>Einzelpreis</span>
+                    <input type="number" className="border-2 rounded-lg ms-2 p-1" {...form.register("unitPrice", {
+                        required: false,
+                        setValueAs: (v) => parseInt(v)
+                    })} />
+                </div>
+
+                <hr className="my-3" />
+
+                <button className="w-full bg-black text-white p-2 rounded-lg font-bold py-2 px-4 hover:bg-gray-700">Add
+                </button>
+            </form>
+        </dialog>
+    </>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 import { ReactToPrint } from "react-to-print";
 import { ChangeEvent, useRef, useState } from "react";
+import Modal from "@/app/components/modal";
+
+const taxRate = 19;
 
 const formatCurrency = (number) => {
     return new Intl.NumberFormat("de-DE", {
@@ -18,24 +21,21 @@ const formatDate = (date) => {
     return new Intl.DateTimeFormat("de-DE").format(date);
 };
 
-interface InvoiceItem {
+export interface InvoiceItem {
     pos: number;
     hours: number;
     unit: string;
     description: string;
     unitPrice: number;
-    totalPrice: number;
 }
 
 interface Invoice {
     invoiceDate: Date;
     invoiceNo: number;
     total: number;
-    subtotal: number;
     hasTax: boolean;
     taxAmount: number;
     hasHours: boolean;
-    items: InvoiceItem[];
 }
 
 function InvoiceTableRow({ item, hasHours }: { item: InvoiceItem, hasHours: boolean }) {
@@ -45,76 +45,121 @@ function InvoiceTableRow({ item, hasHours }: { item: InvoiceItem, hasHours: bool
         <td className="text-center min-w-[70px]">{item.unit}</td>
         <td className="text-[#0070c0] text-start min-w-[60px]">{item.description}</td>
         <td className="text-[#0070c0] text-end">{formatCurrency(item.unitPrice)}</td>
-        <td className="text-[#0070c0] text-end pe-2">{formatCurrency(item.totalPrice)}</td>
+        <td className="text-[#0070c0] text-end pe-2">{formatCurrency(hasHours ? item.unitPrice * item.unitPrice : item.unitPrice)}</td>
     </tr>;
 }
 
 export default function Home() {
     const contentRef = useRef<HTMLElement | null>(null);
 
-    const [data, setData] = useState<Invoice>({
+    const [invoice, setInvoice] = useState<Invoice>({
         invoiceDate: new Date(2021, 11, 17),
         invoiceNo: 240005,
         total: 2975,
         subtotal: 2500,
         hasTax: true,
         taxAmount: 475,
-        hasHours: true,
-        items: [
-            {
-                pos: 1,
-                hours: 50,
-                unit: "Std.",
-                description: "Installation services",
-                unitPrice: 50,
-                totalPrice: 2500
-            }
-        ]
+        hasHours: true
     });
+
+    const [invoiceItems, setInvoiceItems] = useState<InvoiceItem[]>([
+        {
+            pos: 1,
+            hours: 50,
+            unit: "Std.",
+            description: "Installation services",
+            unitPrice: 50
+        }
+    ]);
+
+    const updateSummary = (inv, items) => {
+        inv.total = items.reduce((prev, cur) => {
+            return prev + (inv.hasHours ? cur.unitPrice * cur.hours : cur.unitPrice);
+        }, 0);
+
+        if (inv.hasTax) {
+            inv.taxAmount = inv.total * (taxRate / 100);
+        } else {
+            inv.taxAmount = 0;
+        }
+    };
 
     const handleInvoiceNoChange = (e: ChangeEvent<HTMLDivElement>) => {
         const el = e.target as HTMLDivElement;
-        setData({ ...data, ...{ invoiceNo: parseInt(el.innerText) } });
+        setInvoice({ ...invoice, ...{ invoiceNo: parseInt(el.innerText) } });
     };
 
     const handleTableColHoursToggle = (e: ChangeEvent<HTMLInputElement>) => {
         const el = e.target as HTMLInputElement;
-        setData({ ...data, ...{ hasHours: el.checked } });
+        invoice.hasHours = el.checked;
+
+        updateSummary(invoice, invoiceItems);
+        setInvoice({ ...invoice });
     };
 
     const handleTaxToggle = (e: ChangeEvent<HTMLInputElement>) => {
         const el = e.target as HTMLInputElement;
-        setData({ ...data, ...{ hasTax: el.checked } });
+        invoice.hasTax = el.checked;
+
+        updateSummary(invoice, invoiceItems);
+        setInvoice({ ...invoice });
+    };
+
+    const handleAdd = (record: InvoiceItem) => {
+        const newInvoiceItems = [...invoiceItems, {
+            pos: record.pos,
+            hours: record.hours,
+            unit: record.unit,
+            description: record.description,
+            unitPrice: record.unitPrice
+        }];
+
+        updateSummary(invoice, newInvoiceItems);
+
+        setInvoice({ ...invoice });
+        setInvoiceItems(newInvoiceItems);
     };
 
     return (
         <>
-            <title>{data.invoiceNo}</title>
+            <title>{invoice.invoiceNo}</title>
             <main className="bg-white text-black grid font-primary font-normal">
-                <div className="sticky top-0 mb-5 flex justify-center w-full bg-gray-200 p-4">
-                    <div className="flex gap-5 justify-between">
-                        <button
-                            className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+ Add
-                        </button>
+                <div className="sticky top-0 mb-5 flex justify-center w-full bg-gray-200 p-4 print:hidden">
+                    <div className="flex gap-5 justify-between items-center">
+                        <div>
+                            <label className="flex gap-1 items-center">
+                                <input type="checkbox" onChange={handleTableColHoursToggle}
+                                       defaultChecked={invoice.hasHours} />
+                                Hours
+                            </label>
+
+                            <label className="flex gap-1 items-center">
+                                <input type="checkbox" onChange={handleTaxToggle}
+                                       defaultChecked={invoice.hasTax} />
+                                Tax
+                            </label>
+                        </div>
+
+                        <Modal onAdd={handleAdd} newPos={invoiceItems.length + 1} />
 
                         <ReactToPrint
                             trigger={() => <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Print</button>}
+                                className="w-full bg-blue-500 text-white p-2 rounded-lg font-bold py-2 px-4 hover:bg-blue-700">Print</button>}
                             content={() => contentRef.current}
                         />
                     </div>
                 </div>
-                <section className="w-a4 h-a4 mx-auto" id="container" ref={contentRef}>
+                <section className="w-a4 h-a4 mx-auto py-4" id="container" ref={contentRef}>
                     <div className="w-[640px] m-auto">
-                        <div className="flex justify-end  text-sm">
+                        <div className="flex justify-end text-sm">
                             <div className="text-end">
-                                <div>Datum : <span contentEditable={true}>{formatDate(data.invoiceDate)}</span></div>
+                                <div>Datum : <span contentEditable={true}>{formatDate(invoice.invoiceDate)}</span></div>
                                 <div className="flex gap-1">
                                     <span>Rechnungs-Nr.:</span>
                                     <div contentEditable={true}
                                          onInput={handleInvoiceNoChange}
                                          dangerouslySetInnerHTML={{
-                                             __html: data.invoiceNo
+                                             __html: invoice.invoiceNo
                                          }}></div>
                                 </div>
                             </div>
@@ -143,12 +188,8 @@ export default function Home() {
                                 <thead>
                                 <tr className="bg-[#e7e6e6] text-left px-2">
                                     <th className="text-center px-1 font-normal">Pos.</th>
-                                    <th className={`text-center font-normal ${data.hasHours ? "" : "print:hidden"}`}>
-                                        <div className="flex items-center">
-                                            <input type="checkbox" onChange={handleTableColHoursToggle}
-                                                   defaultChecked={data.hasHours} className="print:hidden" />
-                                            <span className={data.hasHours ? "" : "line-through"}>Anzahl</span>
-                                        </div>
+                                    <th className={`text-center font-normal ${invoice.hasHours ? "" : "print:hidden"}`}>
+                                        <span className={invoice.hasHours ? "" : "line-through"}>Anzahl</span>
                                     </th>
                                     <th className="text-center font-normal">Einheit</th>
                                     <th className="w-max text-start font-normal">Bezeichnung</th>
@@ -158,45 +199,45 @@ export default function Home() {
                                 </thead>
 
                                 <tbody>
-                                {data.items.map((item) => <InvoiceTableRow item={item} hasHours={data.hasHours}
-                                                                           key={item.pos} />)}
+                                {invoiceItems.map((item) => <InvoiceTableRow item={item} hasHours={invoice.hasHours}
+                                                                             key={item.pos} />)}
                                 </tbody>
                             </table>
 
                             <div className="grid gap-3 mb-5 mt-[2.5rem]">
                                 <div className="flex justify-between px-2">
                                     <span>Summe</span>
-                                    <span className="text-[#0070c0]">{formatCurrency(data.subtotal)}</span>
+                                    <span className="text-[#0070c0]">{formatCurrency(invoice.total)}</span>
                                 </div>
 
 
-                                <div className={`flex justify-between px-2 ${data.hasTax ? "" : "print:hidden"}`}>
-                                    <div className="flex gap-1">
-                                        <input type="checkbox" onChange={handleTaxToggle}
-                                               defaultChecked={data.hasTax} className="print:hidden" />
+                                {invoice.hasTax && (
+                                    <div
+                                        className={`flex justify-between px-2 ${invoice.hasTax ? "" : "print:hidden"}`}>
                                         <span>Mehrwertsteuer 19% auf den Nettobedivag</span>
+                                        <span className="text-[#0070c0]">{formatCurrency(invoice.taxAmount)}</span>
                                     </div>
-                                    <span className="text-[#0070c0]">{formatCurrency(data.taxAmount)}</span>
-                                </div>
+                                )}
 
                                 <div className="bg-[#e7e6e6] flex justify-between px-2">
                                     <span className="">Gesamtbedivag</span>
-                                    <span className="text-[#0070c0] font-bold">{formatCurrency(data.total)}</span>
+                                    <span
+                                        className="text-[#0070c0] font-bold">{formatCurrency(invoice.total + invoice.taxAmount)}</span>
                                 </div>
                             </div>
                         </div>
 
                         <div className="px-1">
                             <div className="grid gap-5">
-                                {!data.hasTax &&
+                                {!invoice.hasTax &&
                                     <div contentEditable={true}>Der Leistungsempfänger schuldet die Umsatzsteuer nach §
                                         13b UStG.</div>}
 
                                 <div className="flex gap-1">
                                     <div contentEditable={true}>Der Rechnungsbetrag</div>
-                                    <div>({formatCurrencyWithEurPostfix(data.total)})</div>
+                                    <div>({formatCurrencyWithEurPostfix(invoice.total + invoice.taxAmount)})</div>
                                     <div contentEditable={true}>ist fällig bis zum</div>
-                                    <div contentEditable={true}>{formatDate(data.invoiceDate)}.</div>
+                                    <div contentEditable={true}>{formatDate(invoice.invoiceDate)}.</div>
                                 </div>
                                 <div>
                                     <div>Mit freundlichen Grüßen.</div>
