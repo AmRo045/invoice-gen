336252f1c8c34f7ac3646f999c00629cf2a9659f
AmRo <VirtualEmailBox045@Gmail.Com>
2024-07-31 12:28:48 +0330

    Fix assets prefix issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     output: "export",
     distDir: "build",
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    assetPrefix: isProd ? "https://amro045.github.io/invoice-gen" : undefined
 };
 
 export default nextConfig;
